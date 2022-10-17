afb52de2be4f91b84c4eb4c469c49b5f6d74449f
Sebastian Rivera <sebastianr@myprot.net>
2022-10-17 00:55:03 -0400

    Updated User.create method to pass along correct number of params

--- tests/test_Job.py
+++ tests/test_Job.py
@@ -2,12 +2,15 @@ import pytest
 from lib.User import User
 from lib.Job import Job
 
+
 class TestClass:
 
     def testCreate(self):
         user = User(None)
-        newUserId = user.create("user", "Pass123!", "John", "Doe")
+        newUserId = user.create("user", "Pass123!", "John", "Doe", "USF", "CS")
         job = Job()
-        newJobId = job.create("Software Developer", "program stuff", "Google", "San Francisco, CA", "500000", newUserId)
+        newJobId = job.create("Software Developer", "program stuff",
+                              "Google", "San Francisco, CA", "500000", newUserId)
         assert job.findOne(newJobId) != None
-        assert job.findOne(0) == None # findOne(0) should always return None since database IDs start counting at 1
+        # findOne(0) should always return None since database IDs start counting at 1
+        assert job.findOne(0) == None
--- tests/test_User.py
+++ tests/test_User.py
@@ -1,10 +1,12 @@
 import pytest
 from lib.User import User
 
+
 class TestClass:
 
     def testCreate(self):
         user = User(None)
-        newUserId = user.create("user", "Pass123!", "John", "Doe")
+        newUserId = user.create("user", "Pass123!", "John", "Doe", "USF", "CS")
         assert user.findOne(newUserId) != None
-        assert user.findOne(0) == None # findOne(0) should always return None since database IDs start counting at 1
+        # findOne(0) should always return None since database IDs start counting at 1
+        assert user.findOne(0) == None
