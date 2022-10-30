6a4a993898b0b11d1dcc95fc0cf61af83dd3987d
Sebastian Rivera <sebastianriv@usf.edu>
2022-10-30 19:37:53 -0400

    IN-93 Implemented testCreateApplicationAndHasApplied method in test_Job.py

--- tests/test_Job.py
+++ tests/test_Job.py
@@ -7,10 +7,25 @@ class TestClass:
 
     def testCreate(self):
         user = User(None)
-        newUserId = user.create("user", "Pass123!", "John", "Doe", "USF", "CS")
+        newUserId = user.create("user", "Pass123!", "John", "Doe")
         job = Job()
         newJobId = job.create("Software Developer", "program stuff",
                               "Google", "San Francisco, CA", "500000", newUserId)
         assert job.findOne(newJobId) != None
         # findOne(0) should always return None since database IDs start counting at 1
         assert job.findOne(0) == None
+
+    def testCreateApplicationAndHasApplied(self):
+        user = User(None)
+        newUserId = user.create("user2", "Pass123!",
+                                "John", "Doe")
+        userToApply = User(None)
+        newUserToApplyId = userToApply.create(
+            "user3", "Pass123!", "John", "Doe")
+        job = Job()
+        newJobId = job.create("Software Developer", "program stuff",
+                              "Google", "San Francisco, CA", "500000", newUserId)
+        assert job.hasApplied(newJobId, newUserToApplyId) == False
+        applicationId = job.createApplication("2023", "2023-01-01",
+                                              "lorem ipsum", newUserToApplyId, newJobId)
+        assert job.hasApplied(newJobId, newUserToApplyId) == True
