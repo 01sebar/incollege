5fb1041e954d81d261d9d96ad4454932d4a9cd57
Sebastian Rivera <sebastianriv@usf.edu>
2022-11-06 15:56:27 -0500

    IN-110 Implemented test_Messaging.testSendMessage()

--- tests/Test_Messaging.py
+++ tests/Test_Messaging.py
@@ -1,5 +1,6 @@
 import pytest
 from lib.User import User
+from lib.Message import Message
 
 
 class TestClass:
@@ -12,9 +13,23 @@ class TestClass:
         user.createDefaultSettings()
 
     def testSendMessage(self):
-        # TO-DO create 2 users
+        # Create 2 users
         # One regular, one premium
         # Check count of regular inbox
         # Have Premium send message to regular
         # Verify count of regular inbox == 1
-        print("")
+        userRegular = User(None)
+        userRegularId = userRegular.create(
+            "user1", "Pass123!", "Joe", "Momma", 0)
+        userPremium = User(None)
+        userPremiumId = userPremium.create(
+            "user2", "Pass123!", "Joe", "Momma", 1)
+
+        # Check if premium user can send message to regular
+        messageFromRegularToPremium = Message(userRegularId)
+        assert len(messageFromRegularToPremium.getMessages()) == 0
+        messageFromPremiumToRegular = Message(userPremiumId)
+        messageFromPremiumToRegular.createMessage(
+            userRegularId, "message body")
+        # Check if message received
+        assert len(messageFromRegularToPremium.getMessages()) == 1
