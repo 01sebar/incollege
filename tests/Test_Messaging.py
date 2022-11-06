fde7c868926dbc4e35a0c47760ba09a0658cc8bc
Gianni Rivera <70730828+giannirivera@users.noreply.github.com>
2022-11-06 18:18:35 -0500

    created test_messaging.testUserType()
    passed pytest

--- tests/Test_Messaging.py
+++ tests/Test_Messaging.py
@@ -7,9 +7,17 @@ class TestClass:
     def testUserType(self):
         # TO-DO check user type
         user = User(None)
-        newUserId = user.create(
+        user2 = User(None)
+
+        user.create(
             "user1", "Pass123!", "Joe", "Momma", 0)
         user.createDefaultSettings()
+        assert user.getUsertype() == 0
+
+        user2.create(
+            "user1", "Pass123!", "Joe", "Momma", 1)
+        user2.createDefaultSettings()
+        assert user2.getUsertype() == 1
 
     def testSendMessage(self):
         # TO-DO create 2 users
