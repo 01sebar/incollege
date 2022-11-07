69ce5abbd8125fc8ad68b6d2946099940648806a
Gianni Rivera <70730828+giannirivera@users.noreply.github.com>
2022-11-06 19:01:13 -0500

    updates test_Messaging. testUserType()

--- tests/test_Messaging.py
+++ tests/test_Messaging.py
@@ -6,11 +6,20 @@ from lib.Message import Message
 class TestClass:
 
     def testUserType(self):
-        # TO-DO check user type
+        #check user type
         user = User(None)
-        newUserId = user.create(
+        user2 = User(None)
+        
+        user.create(
             "user1", "Pass123!", "Joe", "Momma", 0)
         user.createDefaultSettings()
+        assert user.getUsertype() == 0 #checks if user is a standard user 
+
+        user2.create(
+            "user1", "Pass123!", "Joe", "Momma", 1)
+        user2.createDefaultSettings()
+        assert user2.getUsertype() == 1 #checks if user is a premium user 
+
 
     def testSendMessage(self):
         # Create 2 users
