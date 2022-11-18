1cc4cde293992dbd9c86ca58ef3a3f261465f6c7
Sebastian Rivera <sebastianriv@usf.edu>
2022-11-18 18:45:00 -0500

    Created Boilerplate Api.py file

---
+++ lib/Api.py
@@ -0,0 +1,22 @@
+class Api:
+
+    def readStudentAccountsFile():
+        return None
+    
+    def readNewJobsFile():
+        return None
+    
+    def writeMyCollegeJobsFile():
+        return None
+    
+    def writeMyCollegeProfilesFile():
+        return None
+    
+    def writeMyCollegeUsersFile():
+        return None
+    
+    def writeMyCollegeAppliedJobsFile():
+        return None
+    
+    def writeMyCollegeSavedJobsFile():
+        return None
